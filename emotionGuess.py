fe9d56ffa50f7a02ce3512007b318118c974feb3
Software Mechanic <softwaremechanic32@gmail.com>
2014-12-23 15:38:12 +0530

    some changes

--- emotionGuess.py
+++ emotionGuess.py
@@ -1,8 +1,8 @@
 import argparse
 import sys
-import facedetect as fdmod
 sys.path.append('/home/anand/Downloads/devbox_configs/')
 from backend import mine as c45Miner
+from backend import fdmod
 import json
 import logging
 import cv2
@@ -10,7 +10,6 @@ import cv
 import csv
 import os
 import numpy
-import statsmodels
 
 logger = logging.getLogger()
 parser = argparse.ArgumentParser(description='Run wishlist python flask app')
@@ -30,6 +29,7 @@ CANNY_HIGH_THRESH = 15.0
 
 
 def getImageAreas(imgFile):
+    # TODO: run nude.py to detect the skin regions before runing facedetect
     featureData = dict()
     FD = fdmod.FeatureDetect()
     image = cv2.imread(os.path.join('./', 'data', imgFile), cv.CV_LOAD_IMAGE_COLOR)
@@ -39,13 +39,13 @@ def getImageAreas(imgFile):
     FD.detectFace()
     FD.detectEyes()
     FD.detectLips()
+    FD.detectNudeAreas()
     featureData.update({imgFile:FD.features})
     return featureData
 
 def getCannyEdges(imgFile):
 
     image = cv.LoadImage(os.path.join('./', 'data', imgFile), cv.CV_LOAD_IMAGE_COLOR)
-    #outImageFile = os.path.join(args.outFolder, 'data', 'edges', imgFile)
     edgeImage = cv.CreateImage((250, 250), 8,1)
     grayImage = cv.CreateImage((250, 250), 8,1)
     cv.CvtColor(image, grayImage, cv.CV_BGR2GRAY)
@@ -103,7 +103,6 @@ def offline_train(args):
     with open('allFeaturesData.csv', 'wb') as csv_fd:
         csvWriter = csv.DictWriter(csv_fd, fieldnames=fields)
         csvWriter.writeheader()
-
         for key in trainingData.keys():
             eyeSidesEdges = dict()
             lipSidesEdges = dict()
@@ -135,6 +134,7 @@ def offline_train(args):
             chin =(calcAreas.get(key).get('lipCorners')[2], calcAreas.get(key).get('lipCorners')[1],
                     calcAreas.get(key).get('lipCorners')[0], calcAreas.get(key).get('faceCorners')[3])
 
+            print  forehead, eyeSidesLeft, eyeSidesRight, lipSidesLeft, lipSidesRight , chin
             # Get the edgeImage and count the edges.
 
             edgeImage = getCannyEdges(key)
@@ -169,10 +169,46 @@ def offline_train(args):
                     c45Input[k].append(v)
     # decisionTree = c45Miner.mine_c45(c45Input, c45Result)
     # rules = c45Miner.tree_to_rules(decisionTree)
+
     # with open('c45Rules', 'wb') as out_fd:
     #     out_fd.write(json.dumps(rules))
     with open('allEdgeCounts.json', 'wb') as out_fd:
         out_fd.write(json.dumps(allEdgeCounts))
+
+def runC45FromFileData():
+    c45Input = dict()
+    c45Result = list()
+    with open('./trainingData.json', 'rb') as inp_fd:
+        trainingData = json.load(inp_fd)
+    with open('./allEdgeCounts.json', 'rb') as inp_fd:
+        allEdgeCounts = json.load(inp_fd)
+    for key in allEdgeCounts.keys():
+        print key, allEdgeCounts.get(key)
+        row = {'filename': key, 'foreheadHorizEdges': allEdgeCounts.get(key).get('foreheadEdges').get('horizontal'),
+                #'foreheadVertEdges': allEdgeCounts.get(key).get('foreheadEdges').get('vertical'),
+                'eyeSidesHorizEdges': allEdgeCounts.get(key).get('eyeSidesEdges').get('horizontal'),
+                'eyeSidesVertEdges': allEdgeCounts.get(key).get('eyeSidesEdges').get('vertical'),
+                'lipSidesHorizEdges': allEdgeCounts.get(key).get('lipSidesEdges').get('horizontal'),
+                'lipSidesVertEdges': allEdgeCounts.get(key).get('lipSidesEdges').get('vertical'),
+                'chinHorizEdges': allEdgeCounts.get(key).get('chinEdges').get('horizontal'),
+                'chinVertEdges': allEdgeCounts.get(key).get('chinEdges').get('vertical'),
+                'emotion': emotionCategories[trainingData.get(key).index(1)]
+                }
+        #emotion = row.pop('emotion')
+        decisionTree = c45Miner.mine_c45(row, 'emotion')
+        rules = c45Miner.tree_to_rules(decisionTree)
+        print decisionTree
+        print rules
+    #    c45Result.append(row.pop('emotion'))
+    #    for k, v in row.iteritems():
+    #        if not c45Input.get(k):
+    #            c45Input[k] = [v]
+    #        else:
+    #            c45Input[k].append(v)
+
+   #  import pdb; pdb.set_trace()
+   #  decisionTree = c45Miner.mine_c45(c45Input, c45Result)
+   #  rules = c45Miner.tree_to_rules(decisionTree)
 
 def training(imgArray, emotions):
     pass
@@ -182,4 +218,5 @@ def testing(imgArray):
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    offline_train(args)
+    #offline_train(args)
+    runC45FromFileData()
